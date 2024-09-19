5983f75fa0f8a098a9fcd8658d02e2279ee55779
seonhoshin <sunlike0508@gmail.com>
2024-09-19 21:38:22 +0900

    초기화

---
+++ src/main/java/hello/jdbc/SpringDatabase1Application.java
@@ -0,0 +1,13 @@
+package hello.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabase1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDatabase1Application.class, args);
+    }
+
+}
